1b8a911d33ec3bc75d0302a11dca571758ef96ab
tientran1234 <tranngoctien29112003@gmail.com>
2025-06-17 09:58:30 +0700

    add module user

--- apps/gateway/src/user.gateway.controller.ts
+++ apps/gateway/src/user.gateway.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Inject, Param, Post } from "@nestjs/common";
+import { Body, Controller, Get, Inject, Param, Patch } from "@nestjs/common";
 import { handleZodError } from "libs/common/helpers";
 import { USER_SERVICE } from "libs/common/src/constants/service-name.constant";
-import { IsPublic } from "libs/common/src/decorator/auth.decorator";
+import { ActiveUser } from "libs/common/src/decorator/active-user.decorator";
+import { ChangePasswordDto, UpdateCustomerProfileDto } from "libs/common/src/request-response-type/customer/customer.model";
 
 import { GetCustomerInformationParamsDTO } from "libs/common/src/request-response-type/user/user.dto";
 import { RawTcpClientService } from "libs/common/src/tcp/raw-tcp-client.service";
@@ -10,8 +11,7 @@ import { RawTcpClientService } from "libs/common/src/tcp/raw-tcp-client.service"
 export class UserGatewayController {
     constructor(
         @Inject(USER_SERVICE) private readonly userRawTcpClient: RawTcpClientService) { }
-    @IsPublic()
-    @Post('get-customer-information/:userId')
+    @Get('get-customer-information/:userId')
     async changeStatusProvider(@Param() params: GetCustomerInformationParamsDTO) {
         try {
             return await this.userRawTcpClient.send({ type: 'GET_CUSTOMER', userId: params.userId, })
@@ -20,4 +20,22 @@ export class UserGatewayController {
         }
 
     }
+
+    @Patch('update-customer-information')
+    async updateCustomer(@Body() body: UpdateCustomerProfileDto, @ActiveUser("userId") userId: number) {
+        try {
+            return await this.userRawTcpClient.send({ type: 'UPDATE_CUSTOMER', data: { ...body }, userId })
+        } catch (error) {
+            handleZodError(error)
+        }
+    }
+    @Patch('change-password')
+    async changePassword(@Body() body: ChangePasswordDto, @ActiveUser("userId") userId: number) {
+        try {
+            return await this.userRawTcpClient.send({ type: 'CHANGE-PASSWORD', data: { ...body }, userId })
+        } catch (error) {
+            handleZodError(error)
+        }
+    }
+
 }
