535c8cb6550d97c3fc132aa17d3e7655df14e702
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-01 22:40:19 +0700

    fix error message

--- libs/common/helpers.ts
+++ libs/common/helpers.ts
@@ -55,6 +55,8 @@ export const adjustDateToWeekday = (startDate: Date, day: WeekDay): Date => {
 
 export function handleZodError(error: any): ErrorResponse {
     const errorRes = error.error.response || error.response
+    console.log(errorRes);
+
     if (errorRes) {
 
         const status = error.status ? error.status : error.response.statusCode
