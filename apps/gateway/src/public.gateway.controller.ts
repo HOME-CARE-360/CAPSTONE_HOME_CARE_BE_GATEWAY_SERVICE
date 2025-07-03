38e0aacd8c62dce44555293bd7af8bad94414d8b
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-03 19:02:42 +0700

    test error response

--- apps/gateway/src/public.gateway.controller.ts
+++ apps/gateway/src/public.gateway.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Get, Inject, Param, Patch } from "@nestjs/common";
-import { handleZodError } from "libs/common/helpers";
+import { Body, Controller, Get, HttpException, Inject, Param, Patch } from "@nestjs/common";
+import { handlerErrorResponse, handleZodError } from "libs/common/helpers";
 import { USER_SERVICE } from "libs/common/src/constants/service-name.constant";
 import { ActiveUser } from "libs/common/src/decorator/active-user.decorator";
 import { ChangePasswordDTO } from "libs/common/src/request-response-type/customer/customer.dto";
@@ -44,8 +44,10 @@ export class PublicGatewayController {
         try {
             const data = await this.userRawTcpClient.send({ type: 'CHANGE_PASSWORD', data: { ...body }, userId })
             console.log(data);
+            handlerErrorResponse(data)
             return data
         } catch (error) {
+            if (error instanceof HttpException) throw error;
             handleZodError(error)
         }
     }
