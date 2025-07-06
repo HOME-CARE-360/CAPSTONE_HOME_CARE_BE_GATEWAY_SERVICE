3f88a8a96923697a8f9ac8aa6f64e70c3669c00a
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-06 11:52:46 +0700

    add module admin

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -4,7 +4,7 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AuthGatewayController } from './auth.gateway.controller';
 import { CommonModule } from 'libs/common/src';
 import { ConfigModule } from 'libs/common/src/modules/config.module';
-import { AUTH_SERVICE, BOOKING_SERVICE, MANAGER_SERVICE, MEDIA_SERVICE, PROVIDER_SERVICE, SERVICE_SERVICE, STAFF_SERVICE, USER_SERVICE } from 'libs/common/src/constants/service-name.constant';
+import { ADMIN_SERVICE, AUTH_SERVICE, BOOKING_SERVICE, MANAGER_SERVICE, MEDIA_SERVICE, PROVIDER_SERVICE, SERVICE_SERVICE, STAFF_SERVICE, USER_SERVICE } from 'libs/common/src/constants/service-name.constant';
 import { ManagerGatewayController } from './manager.gateway.controller';
 import { MediaGatewayController } from './media.gateway.controller';
 import { APP_PIPE } from '@nestjs/core';
@@ -98,6 +98,14 @@ import { AdminGatewayController } from './admin.gateway.controller';
     useFactory: () => {
       const host = process.env.STAFF_HOST || 'localhost';
       const port = parseInt(process.env.STAFF_TCP_PORT || '4002');
+      return new RawTcpClientService(host, port);
+    },
+  },
+  {
+    provide: ADMIN_SERVICE,
+    useFactory: () => {
+      const host = process.env.ADMIN_POD_HOST || 'localhost';
+      const port = parseInt(process.env.ADMIN_POD_TCP_PORT || '4003');
       return new RawTcpClientService(host, port);
     },
   }]
