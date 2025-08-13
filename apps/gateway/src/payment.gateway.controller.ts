2736214998b39b17d63a0a52b987dd345276bd2b
uncreasing-rise <truonghoangprovip@gmail.com>
2025-08-13 11:26:36 +0700

    Fix: Webhook

--- apps/gateway/src/payment.gateway.controller.ts
+++ apps/gateway/src/payment.gateway.controller.ts
@@ -53,45 +53,62 @@ export class PaymentGatewayController {
       handleZodError(error);
     }
   }
-
-  @IsPublic()
-  @Post('callback')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        orderCode: { type: 'string', example: '1234567890' },
-        status: { type: 'string', enum: ['PAID', 'FAILED'], example: 'PAID' },
-      },
-      required: ['orderCode', 'status'],
+@IsPublic()
+@Post('callback')
+@ApiBody({
+  schema: {
+    type: 'object',
+    properties: {
+      orderCode: { type: 'string', example: '1234567890' },
+      status: { type: 'string', enum: ['PAID', 'FAILED'], example: 'PAID' },
     },
-  })
-  async handlePayOSCallback(@Body() payload: any) {
-    if (!payload.orderCode || !payload.status) {
-      throw new HttpException(
-        {
-          success: false,
-          message: 'Missing orderCode or status in callback payload',
-          error: 'Bad Request',
-        },
-        400,
-      );
-    }
-    try {
-      const data = await this.paymentRawTcpClient.send({
-        type: 'HANDLE_PAYOS_CALLBACK',
-        data: {
-          orderCode: payload.orderCode,
-          status: payload.status,
-        },
-      });
-      handlerErrorResponse(data);
-      return data;
-    } catch (error) {
-      if (error instanceof HttpException) throw error;
-      handleZodError(error);
-    }
+    required: ['orderCode', 'status'],
+  },
+})
+async handlePayOSCallback(@Body() payload: any) {
+  
+ const orderCodeStr = String(payload.data.orderCode);
+
+console.log('Received PayOS callback:', orderCodeStr, 'Type:', typeof orderCodeStr);
+
+let status: 'PAID' | 'FAILED';
+if (payload.code === '00') {
+  status = 'PAID';
+} else {
+  status = 'FAILED';
+}
+
+console.log(`Payment status determined: ${status}`, 'Type:', typeof status);
+
+  if (!payload.data.orderCode) {
+    throw new HttpException(
+      {
+        success: false,
+        message: 'Invalid callback payload',
+        error: 'Bad Request',
+      },
+      400,
+    );
   }
+
+  
+
+  try {
+    const data = await this.paymentRawTcpClient.send({
+      type: 'HANDLE_PAYOS_CALLBACK',
+      data: {
+        orderCode: payload.data.orderCode,
+        status,
+      },
+    });
+    handlerErrorResponse(data);
+    return data;
+  } catch (error) {
+    if (error instanceof HttpException) throw error;
+    handleZodError(error);
+  }
+}
+
 
   @Post('create-proposal-transaction')
   @ApiBody({
