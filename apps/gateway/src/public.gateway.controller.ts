882436bdd4f05e8fcf1de4af78b2468015e200b9
uncreasing-rise <truonghoangprovip@gmail.com>
2025-08-14 15:32:45 +0700

    Fix Public

--- apps/gateway/src/public.gateway.controller.ts
+++ apps/gateway/src/public.gateway.controller.ts
@@ -10,6 +10,7 @@ import {
 import { handlerErrorResponse, handleZodError } from 'libs/common/helpers';
 import { USER_SERVICE } from 'libs/common/src/constants/service-name.constant';
 import { ActiveUser } from 'libs/common/src/decorator/active-user.decorator';
+import { IsPublic } from 'libs/common/src/decorator/auth.decorator';
 import { ChangePasswordDTO } from 'libs/common/src/request-response-type/customer/customer.dto';
 
 import { RawTcpClientService } from 'libs/common/src/tcp/raw-tcp-client.service';
@@ -21,6 +22,7 @@ export class PublicGatewayController {
     @Inject(USER_SERVICE)
     private readonly userRawTcpClient: RawTcpClientService,
   ) {}
+  @IsPublic()
   @Get('get-staff-information/:staffId')
   async getStaffInformation(@Param('staffId') staffId: number) {
     try {
@@ -35,6 +37,8 @@ export class PublicGatewayController {
       handleZodError(error);
     }
   }
+
+  @IsPublic()
   @Get('get-customer-information/:customerId')
   async changeStatusProvider(@Param('customerId') customerId: number) {
     try {
@@ -49,6 +53,8 @@ export class PublicGatewayController {
       handleZodError(error);
     }
   }
+
+  @IsPublic()
   @Get('get-service-provider-information/:providerId')
   async getServiceProviderInformation(@Param('providerId') providerId: number) {
     try {
@@ -63,6 +69,7 @@ export class PublicGatewayController {
       handleZodError(error);
     }
   }
+
   @Patch('change-password')
   async changePassword(
     @Body() body: ChangePasswordDTO,
@@ -82,6 +89,7 @@ export class PublicGatewayController {
       handleZodError(error);
     }
   }
+  
   @Get('get-me')
   async getMe(@ActiveUser() user: AccessTokenPayload) {
     const keyName = ((user.customerId && 'customerId') ||
