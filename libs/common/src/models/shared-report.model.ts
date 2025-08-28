6b4892f74299f2f4bfbe311ce093e6004657bb8e
tientran1234 <tranngoctien29112003@gmail.com>
2025-08-29 01:07:16 +0700

    fix bug param

--- libs/common/src/models/shared-report.model.ts
+++ libs/common/src/models/shared-report.model.ts
@@ -1,34 +1,25 @@
-import { ReportStatus } from '@prisma/client';
-import { z } from 'zod';
 
-export enum ReporterType {
-  CUSTOMER = 'CUSTOMER',
-  PROVIDER = 'PROVIDER',
-}
+import { ReporterType, ReportStatus } from "@prisma/client"
+import { z } from "zod"
 
 export const BookingReportSchema = z.object({
-  id: z.number(),
-  bookingId: z.number(),
-  reporterId: z.number(),
-  reporterType: z.enum([ReporterType.CUSTOMER, ReporterType.PROVIDER]),
-  reportedCustomerId: z.number().nullable(),
-  reportedProviderId: z.number().nullable(),
+    id: z.coerce.number().int().positive(),
+    bookingId: z.number(),
+    reporterId: z.number(),
+    reporterType: z.enum([ReporterType.CUSTOMER, ReporterType.PROVIDER]),
+    reportedCustomerId: z.number().nullable(),
+    reportedProviderId: z.number().nullable(),
 
-  reason: z.string().max(255),
-  description: z.string().max(2000).nullable(),
-  imageUrls: z.array(z.string()),
+    reason: z.string().max(255),
+    description: z.string().max(2000).nullable(),
+    imageUrls: z.array(z.string()),
 
-  status: z.enum([
-    ReportStatus.PENDING,
-    ReportStatus.RESOLVED,
-    ReportStatus.UNDER_REVIEW,
-    ReportStatus.REJECTED,
-  ]),
-  createdAt: z.date(),
-  reviewedAt: z.date().nullable(),
-  reviewedById: z.number().nullable(),
-  note: z.string().max(1000).nullable(),
-  reviewResponse: z.string().max(2000).nullable(),
-});
+    status: z.enum([ReportStatus.PENDING, ReportStatus.RESOLVED, ReportStatus.UNDER_REVIEW, ReportStatus.REJECTED]),
+    createdAt: z.date(),
+    reviewedAt: z.date().nullable(),
+    reviewedById: z.number().nullable(),
+    note: z.string().max(1000).nullable(),
+    reviewResponse: z.string().max(2000).nullable(),
+})
 
 export type ProviderReportType = z.infer<typeof BookingReportSchema>;
